e45e45ed5c8ec7aef52a735e11a7c6bf73e56ee0
Bob Forcha <bobforcha@gmail.com>
2021-12-09 22:58:03 -0500

    get peak filter response curve working

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -27,11 +27,26 @@ SimpleEQAudioProcessorEditor::SimpleEQAudioProcessorEditor (SimpleEQAudioProcess
       addAndMakeVisible(comp);
     }
 
+    const auto& params = audioProcessor.getParameters();
+
+    for(auto param : params)
+    {
+      param->addListener(this);
+    }
+
+    startTimerHz(60);
+
     setSize (600, 400);
 }
 
 SimpleEQAudioProcessorEditor::~SimpleEQAudioProcessorEditor()
 {
+  const auto& params = audioProcessor.getParameters();
+
+  for(auto param : params)
+  {
+    param->removeListener(this);
+  }
 }
 
 //==============================================================================
@@ -138,7 +153,11 @@ void SimpleEQAudioProcessorEditor::timerCallback()
   if(parametersChanged.compareAndSetBool(false, true))
   {
     // update the mono chain
+    auto chainSettings = getChainSettings(audioProcessor.apvts);
+    auto peakCoefficients = makePeakFilter(chainSettings, audioProcessor.getSampleRate());
+    updateCoefficients(monoChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
     // signal a repaint
+    repaint();
   }
 }
 
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -44,6 +44,45 @@ using Coefficients = Filter::CoefficientsPtr;
 void updateCoefficients(Coefficients& old, const Coefficients& replacements);
 
 Coefficients makePeakFilter(const ChainSettings& chainSettings, double samplRate);
+
+template<int Index, typename ChainType, typename CoefficientType>
+void update(ChainType& chain, const CoefficientType& coefficients)
+{
+  updateCoefficients(chain.template get<Index>().coefficients, coefficients[Index]);
+  chain.template setBypassed<Index>(false);
+}
+
+template<typename ChainType, typename CoefficientType>
+void updateCutFilter(ChainType& leftLowCut,
+  const CoefficientType& cutCoefficients,
+  const Slope& lowCutSlope)
+  {
+    leftLowCut.template setBypassed<0>(true);
+    leftLowCut.template setBypassed<1>(true);
+    leftLowCut.template setBypassed<2>(true);
+    leftLowCut.template setBypassed<3>(true);
+
+    switch (lowCutSlope)
+    {
+      case Slope_48:
+      {
+        update<3>(leftLowCut, cutCoefficients);
+      }
+      case Slope_36:
+      {
+        update<2>(leftLowCut, cutCoefficients);
+      }
+      case Slope_24:
+      {
+        update<1>(leftLowCut, cutCoefficients);
+      }
+      case Slope_12:
+      {
+        update<0>(leftLowCut, cutCoefficients);
+      }
+    }
+  }
+
 //==============================================================================
 /**
 */
@@ -99,44 +138,6 @@ private:
   MonoChain leftChain, rightChain;
 
   void updatePeakFilter(const ChainSettings& chainSettings);
-
-  template<int Index, typename ChainType, typename CoefficientType>
-  void update(ChainType& chain, const CoefficientType& coefficients)
-  {
-    updateCoefficients(chain.template get<Index>().coefficients, coefficients[Index]);
-    chain.template setBypassed<Index>(false);
-  }
-
-  template<typename ChainType, typename CoefficientType>
-  void updateCutFilter(ChainType& leftLowCut,
-    const CoefficientType& cutCoefficients,
-    const Slope& lowCutSlope)
-  {
-    leftLowCut.template setBypassed<0>(true);
-    leftLowCut.template setBypassed<1>(true);
-    leftLowCut.template setBypassed<2>(true);
-    leftLowCut.template setBypassed<3>(true);
-
-    switch (lowCutSlope)
-    {
-      case Slope_48:
-      {
-        update<3>(leftLowCut, cutCoefficients);
-      }
-      case Slope_36:
-      {
-        update<2>(leftLowCut, cutCoefficients);
-      }
-      case Slope_24:
-      {
-        update<1>(leftLowCut, cutCoefficients);
-      }
-      case Slope_12:
-      {
-        update<0>(leftLowCut, cutCoefficients);
-      }
-    }
-  }
 
   void updateLowCutFilters(const ChainSettings& chainSettings);
   void updateHighCutFilters(const ChainSettings& chainSettings);
