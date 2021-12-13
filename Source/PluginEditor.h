2137bc5f1429eea4d264856f71b0c6bf06f82ac3
Bob Forcha <bobforcha@gmail.com>
2021-12-12 23:28:53 -0500

    implemented skeleton for custom slider visuals

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -12,14 +12,41 @@
 #include "PluginProcessor.h"
 
 // Custom rotary slider
-struct CustomRotarySlider : juce::Slider
+struct LookAndFeel : juce::LookAndFeel_V4
 {
-  CustomRotarySlider() : juce::Slider(
-    juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
-    juce::Slider::TextEntryBoxPosition::NoTextBox)
-  {
+  virtual void drawRotarySlider (juce::Graphics&,
+                                  int x, int y, int width, int height,
+                                  float sliderPosProportional,
+                                  float rotaryStartAngle,
+                                  float rotaryEndAngle,
+                                  juce::Slider&) override { }
+};
 
+struct RotarySliderWithLabels : juce::Slider
+{
+  RotarySliderWithLabels(juce::RangedAudioParameter& rap, const juce::String& unitSuffix) :
+  juce::Slider(
+    juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+    juce::Slider::TextEntryBoxPosition::NoTextBox),
+  param(&rap),
+  suffix(unitSuffix)
+  {
+    setLookAndFeel(&lnf);
   }
+
+  ~RotarySliderWithLabels()
+  {
+    setLookAndFeel(nullptr);
+  }
+
+  void paint(juce::Graphics& g) override {}
+  juce::Rectangle<int> getSliderBounds() const;
+  int getTextHeight() const { return 14; }
+  juce::String getDisplayString() const;
+private:
+  LookAndFeel lnf;
+  juce::RangedAudioParameter* param;
+  juce::String suffix;
 };
 
 struct ResponseCurveComponent: juce::Component,
@@ -64,7 +91,7 @@ private:
     SimpleEQAudioProcessor& audioProcessor;
 
     // Add sliders
-    CustomRotarySlider peakFreqSlider,
+    RotarySliderWithLabels peakFreqSlider,
     peakGainSlider,
     peakQualitySlider,
     lowCutFreqSlider,
